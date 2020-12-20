23575e2721e0f1020f00f7f0e8b9fde9fe06028b
maivinhlam <maivinhlam910@gmail.com>
2020-12-20 17:17:41 +0700

    Add migrate

---
+++ database/migrations/2020_12_20_091725_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('product_types');
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->decimal('sell_price', 8, 2);
+            $table->integer('amount');
+            $table->longText('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
